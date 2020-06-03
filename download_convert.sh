4cb19cc7ca09ea04c6afebf37fece4e5b5c27933
Richard Bruskiewich <richard@starinformatics.com>
2020-06-03 11:36:58 -0700

    Updated parameters to latest SemMedDb release

--- download_convert.sh
+++ download_convert.sh
@@ -1,16 +1,52 @@
 #!/usr/bin/env bash
 
-SEMEDDB_DOWNLOAD_PATH=https://skr3.nlm.nih.gov/SemMedDB/download/
-SEMEDDB_VERSION=31
-SEMMEDB_FILE_PREFIX=semmedVER${SEMEDDB_VERSION}
-SEMEDDB_PUBMED_VERSION=to12312017
+echo "Downloading and pre-processing SemMedDb"
 
-SEMMEDDB_PREDICATION_FILE=${SEMMEDB_FILE_PREFIX}_R_PREDICATION_${SEMEDDB_PUBMED_VERSION}
+#
+# SemMedDb 2019 data release values in original Greg Stupp script
+#
+# SEMEDDB_DOWNLOAD_PATH=https://skr3.nlm.nih.gov/SemMedDB/download/
+# SEMEDDB_VERSION=31
+# SEMEDDB_PUBMED_VERSION=to12312017
+#
+# SemMedDb 2020 data release values in June 2020 script
+#
+SEMEDDB_DOWNLOAD_PATH=https://ii.nlm.nih.gov/SemRep_SemMedDB_SKR/SemMedDB/download/
+SEMEDDB_VERSION=42
+SEMEDDB_PUBMED_VERSION=2020
+
+SEMMEDB_FILE_PREFIX=semmedVER${SEMEDDB_VERSION}
+
+# Data file targets:
+
+# SemMedDb 2019 data release file name in original Greg Stupp script
+# SEMMEDDB_PREDICATION_FILE=${SEMMEDB_FILE_PREFIX}_R_PREDICATION_${SEMEDDB_PUBMED_VERSION}
+
+# Slightly revised file name format in SemMedDb 2020 data release
+SEMMEDDB_PREDICATION_FILE=${SEMMEDB_FILE_PREFIX}_${SEMEDDB_PUBMED_VERSION}_R_PREDICATION
+
+# Data Targets:
 SEMMEDDB_PREDICATION_ARCHIVE=${SEMMEDDB_PREDICATION_FILE}.sql.gz
 SEMMEDDB_PREDICATION_DOWNLOAD=${SEMEDDB_DOWNLOAD_PATH}${SEMMEDDB_PREDICATION_ARCHIVE}
 SEMMEDDB_PREDICATION_CSV=${SEMMEDDB_PREDICATION_FILE}.csv
 
-# https://skr3.nlm.nih.gov/SemMedDB/download/download.html
+echo
+echo "SemMedDb Predication File Variables":
+echo
+echo Archive Name:  ${SEMMEDDB_PREDICATION_ARCHIVE}
+echo Download Path: ${SEMMEDDB_PREDICATION_DOWNLOAD}
+echo CSV File Name: ${SEMMEDDB_PREDICATION_CSV}
+
+echo
+echo See the available variables in the shell script to override these values.
+echo
+read -p 'Continue? (y/n): ' RUNSCRIPT
+
+if [[ ${RUNSCRIPT} != 'y' ]]; then echo 'See you later... Goodbye!'; exit 0; fi
+
+echo Downloading ${SEMMEDDB_PREDICATION_ARCHIVE} from ${SEMEDDB_DOWNLOAD_PATH}
+
+# Downloading attempted here!
 wget -N ${SEMMEDDB_PREDICATION_DOWNLOAD}
 
 # prepend colnames
@@ -20,7 +56,7 @@ cp col_names.txt ${SEMMEDDB_PREDICATION_CSV}
 zcat ${SEMMEDDB_PREDICATION_ARCHIVE}| python3 mysqldump_to_csv.py >> ${SEMMEDDB_PREDICATION_CSV}
 
 
-# download umls metathesarous files
+# download umls metathesaurus files
 wget -N "https://download.nlm.nih.gov/umls/kss/2018AA/umls-2018AA-full.zip"
 # I then manually unzipped this file, then extracted the contained files, then put all of the RRF files in one folder ("2018AA-full")
 
