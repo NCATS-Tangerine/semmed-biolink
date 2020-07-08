58d44ff11a60e4c389a1b5985d82d1ec79397855
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2020-07-08 17:58:34 +0100

    roughed in polymorphic sql-to-csv script

---
+++ semmed_sql_to_csv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[  -z ${1} ]]; then
+     echo 'Usage: semmed_sql_to_csv.sh (predications|predications_aux|sentences)';
+     exit 0;
+fi
+
+#if [[  -z ${SEMMEDDB_PREDICATION_ARCHIVE} ]]; then
+#     echo 'Run the setup_environment.sh script before attempting to run this script!';
+#     exit 0;
+#fi
+
+# prepend colnames
+#
+# The latest release has a few (seemingly undocumented?) extra columns: FACT_VALUE,MODE_SCALE,MOD_VALUE
+#
+#cp col_names_2020.txt ${SEMMEDDB_PREDICATION_CSV}
+
+# convert mysqldump to csv
+#gunzip -c ${SEMMEDDB_PREDICATION_ARCHIVE}| python3 mysqldump_to_csv.py >> ${SEMMEDDB_PREDICATION_CSV}
